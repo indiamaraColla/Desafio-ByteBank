6d3ea9874393e300a64f21155f2662fe69e5db07
indiamara.colla <maratsiqueira@gmail.com>
2023-07-04 11:59:47 -0300

    feat: removendo

--- scripts/pullRequestPreview.ts
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-
-console.log('[DEPLOY_PREVIEW]: START');
-const command = 'yarn deploy:staging';
-const output = execSync(command, { encoding: 'utf8' });
-const outputLines = output.split('\n');
-const DEPLOY_URL = outputLines[outputLines.length - 1];
-console.log('[DEPLOY_PREVIEW]: END', DEPLOY_URL);
