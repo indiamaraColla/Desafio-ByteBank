8de960fb2dce4338126d66f8413a5599801d2606
indiamara.colla <maratsiqueira@gmail.com>
2023-07-04 11:36:08 -0300

    feat: uppdate test preview

--- scripts/pullRequestPreview.ts
+++ scripts/pullRequestPreview.ts
@@ -1,1 +1,8 @@
-console.log('Olá mundo');
+import { execSync } from 'child_process';
+
+console.log('[DEPLOY_PREVIEW]: START');
+const command = 'yarn deploy:staging';
+const output = execSync(command, { encoding: 'utf8' });
+const outputLines = output.split('\n');
+const DEPLOY_URL = outputLines[outputLines.length - 1];
+console.log('[DEPLOY_PREVIEW]: END', DEPLOY_URL);
